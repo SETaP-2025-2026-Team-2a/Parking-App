141275cdd07e7a9a709e3448fe29355e5629ba5f
Dynamite-TH <tedhayward@icloud.com>
2026-02-28 19:14:49 +0000

    refactor: update API resource routes and improve structure

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,18 @@
 import flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_cors import CORS
 
 from car_park_manager import ParkingSpots
+from user_manager import register_user_routes
 
 server= flask.Flask(__name__)
 CORS(server) 
 api = Api(server)
 
-api.add_resource(ParkingSpots, '/parking-spots')
-api.add_resource('/login')
+
+
+api.add_resource(ParkingSpots, '/')
+register_user_routes(server)
+
 if __name__ == '__main__':
     server.run(debug=True, host='0.0.0.0', port=8080)
