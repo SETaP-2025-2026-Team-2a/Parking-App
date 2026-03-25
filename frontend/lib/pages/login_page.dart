1ae3e8b9275aa33bdb5c4e3b99a5098dc52e3ada
Joshua Rymer <joshrymer0@gmail.com>
2026-03-25 17:28:13 +0000

    login_page content

---
+++ frontend/lib/pages/login_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+//overall box
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            TextField(decoration: InputDecoration(labelText: 'Email')),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle login logic here
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
