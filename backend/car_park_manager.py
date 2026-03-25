4cc9318d0bba1d455dbd7faf523bc132615f64bf
Oscar Peace <oscar@oscarcp.net>
2026-03-25 11:46:22 +0000

    fix: change parking spots to car park

--- backend/car_park_manager.py
+++ backend/car_park_manager.py
@@ -1,37 +1,33 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from marshmallow import Schema, fields, validate
 
 
 class CarParkSchema(Schema):
+    carpark_id = fields.String()
     name = fields.String()
     spaces = fields.Integer(validate=validate.Range(min=0))
     distance = fields.Float(validate=validate.Range(min=0))
+    avg_rating = fields.Float(validate=validate.Range(min=0))
 
 
-class ParkingSpots(Resource):
+class CarPark(Resource):
     def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("carpark_id", type=int, required=True)
+
+        # TODO
+        # verify car park exists
+        # execute query
+        # return car park
+
         return {
-            "data": [
-                CarParkSchema(
-                    {
-                        "name": "Gunwharf Quays",
-                        "price": 100,
-                        "distance": 10,
-                    }
-                ).dump(),
-                CarParkSchema(
-                    {
-                        "name": "The Hard",
-                        "price": 100,
-                        "distance": 10,
-                    }
-                ).dump(),
-                CarParkSchema(
-                    {
-                        "name": "Clarence Esplanade",
-                        "price": 100,
-                        "distance": 10,
-                    }
-                ).dump(),
-            ]
+            "data": CarParkSchema(
+                {
+                    "carpark_id": 0,
+                    "name": "Gunwharf Quays",
+                    "price": 100,
+                    "distance": 10,
+                    "avg_rating": 1.0,
+                }
+            ).dump()
         }
