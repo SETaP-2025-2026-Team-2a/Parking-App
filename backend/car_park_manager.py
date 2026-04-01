54b5a92e2bd7c8bf06c89ea3ab62fd196f2775b8
Dynamite-TH <tedhayward@icloud.com>
2026-04-01 21:56:02 +0100

    implement GET and POST methods for car park management

--- backend/car_park_manager.py
+++ backend/car_park_manager.py
@@ -1,5 +1,9 @@
+import email
+
 from flask_restful import Resource, reqparse
 from marshmallow import Schema, fields, validate
+
+from backend.user_manager import get_database_connection
 
 
 class CarParkSchema(Schema):
@@ -14,20 +18,40 @@ class CarPark(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument("carpark_id", type=int, required=True)
+        args = parser.parse_args()
+        
+        supabase = get_database_connection()
+        response = supabase.table("car_parks").select("name", "spaces", "distance", "avg_rating").eq("carpark_id", args["carpark_id"]).execute()
+        if response.data:
+            car_park_data = response.data[0]
+            return {
+                "data": CarParkSchema().dump(car_park_data)
+            }, 200
+        return {
+            "error": "Car park not found"
+        }, 404
 
-        # TODO
-        # verify car park exists
-        # execute query
-        # return car park
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", type=str, required=True)
+        parser.add_argument("spaces", type=int, required=True)
+        parser.add_argument("distance", type=float, required=True)
+        parser.add_argument("avg_rating", type=float, required=True)
+        args = parser.parse_args()
+
+        supabase = get_database_connection()
+        response = supabase.table("car_parks").insert({
+            "name": args["name"],
+            "spaces": args["spaces"],
+            "distance": args["distance"],
+            "avg_rating": args["avg_rating"]
+        }).execute()
+
+        if response.error:
+            return {
+                "error": "Failed to create car park"
+            }, 500
 
         return {
-            "data": CarParkSchema(
-                {
-                    "carpark_id": 0,
-                    "name": "Gunwharf Quays",
-                    "price": 100,
-                    "distance": 10,
-                    "avg_rating": 1.0,
-                }
-            ).dump()
-        }
+            "data": CarParkSchema().dump(response.data[0])
+        }, 201
