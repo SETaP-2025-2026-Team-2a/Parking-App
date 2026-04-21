1bdd4179e4f2371d823b7f939575d50b99328f4f
Oscar Peace <oscar@oscarcp.net>
2026-04-21 18:22:35 +0100

    feat: add reviews route to server

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,12 @@
 import flask
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask_cors import CORS
 
 from car_park_manager import CarPark
 from search_manager import SearchManager
 from user_manager import UserResource, UsersResource
 from authentication_manager import LoginResource
+from review_manager import ReviewManager
 
 
 server= flask.Flask(__name__)
@@ -17,6 +18,7 @@ api.add_resource(SearchManager, '/search') # GET search query
 api.add_resource(UsersResource, '/users')  # POST create
 api.add_resource(UserResource, '/users/<string:email>') # GET read, PUT update
 api.add_resource(LoginResource, "/login") # POST login
+api.add_resource(ReviewManager, "/review") # POST add review, GET get reviews for specific car park, DELETE
 
 
 
